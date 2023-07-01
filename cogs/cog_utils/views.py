c583ba95e4ff68d7a79b49b8716bd17c68cc9c45
rtk-rnjn <ritik0ranjan@gmail.com>
2023-07-02 02:28:55 +0530

    Add LICENSE

--- cogs/cog_utils/views.py
+++ cogs/cog_utils/views.py
@@ -1,5 +1,26 @@
-# https://github.com/quotientbot/Quotient-Bot/blob/main/src/cogs/utility/__init__.py#L195-L205
-# commit hash: 177ef9f9ee91970a21b28d9c4124c023c34887f7
+"""
+MIT License
+
+Copyright (c) 2023 Ritik Ranjan
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
 
 from __future__ import annotations
 
